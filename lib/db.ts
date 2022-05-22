b06fc269841cd90e3bd687f564abfae85399658f
SlashNephy <spica@starry.blue>
2022-05-22 16:22:13 +0900

    fix: DB のクリーンアップをする
    Signed-off-by: SlashNephy <spica@starry.blue>

--- lib/db.ts
+++ lib/db.ts
@@ -2,27 +2,39 @@ import { PrismaClient } from '@prisma/client'
 
 import type { SyobocalPage } from '.prisma/client'
 
-const client = new PrismaClient()
+const createClient = () => new PrismaClient()
 
 export const getSyobocalPageFromDatabase = async (tid: number): Promise<SyobocalPage | null> => {
-  return await client.syobocalPage.findUnique({
-    where: {
-      tid,
-    },
-  })
+  const client = createClient()
+
+  try {
+    return await client.syobocalPage.findUnique({
+      where: {
+        tid,
+      },
+    })
+  } finally {
+    await client.$disconnect()
+  }
 }
 
 export const updateSyobocalPageInDatabase = async (tid: number, content: string) => {
-  await client.syobocalPage.upsert({
-    where: {
-      tid,
-    },
-    create: {
-      tid,
-      content,
-    },
-    update: {
-      content,
-    },
-  })
+  const client = createClient()
+
+  try {
+    await client.syobocalPage.upsert({
+      where: {
+        tid,
+      },
+      create: {
+        tid,
+        content,
+      },
+      update: {
+        content,
+      },
+    })
+  } finally {
+    await client.$disconnect()
+  }
 }
