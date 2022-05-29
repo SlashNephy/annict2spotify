86929a8dba368bbba9a28aa66fab5413a9e3dff4
SlashNephy <spica@starry.blue>
2022-05-29 18:36:21 +0900

    fix: テーマが変わらなくなるのを修正
    Signed-off-by: SlashNephy <spica@starry.blue>

--- lib/hooks/useMemorableColorScheme.tsx
+++ lib/hooks/useMemorableColorScheme.tsx
@@ -3,13 +3,24 @@ import { useLocalStorage } from '@mantine/hooks'
 import type { ColorScheme } from '@mantine/core'
 
 export const useMemorableColorScheme = () => {
-  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-    key: 'color-scheme',
-    defaultValue: 'light',
+  const [isDark, setIsDark] = useLocalStorage<boolean>({
+    key: 'is-dark-color-scheme',
+    defaultValue: false,
   })
+
   const toggleColorScheme = (value?: ColorScheme) => {
-    setColorScheme(value || (value === 'dark' ? 'light' : 'dark'))
+    switch (value) {
+      case 'dark':
+        setIsDark(false)
+        break
+      case 'light':
+        setIsDark(true)
+        break
+      default:
+        setIsDark((previous) => !previous)
+    }
   }
 
+  const colorScheme = isDark ? 'dark' : 'light'
   return [colorScheme, toggleColorScheme] as const
 }
