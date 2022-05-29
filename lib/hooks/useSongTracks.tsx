8bca30a3fc2190c1f4536bf1ec751cf59d77080b
SlashNephy <spica@starry.blue>
2022-05-29 14:17:42 +0900

    fix: dynamic import
    Signed-off-by: SlashNephy <spica@starry.blue>

--- lib/hooks/useSongTracks.tsx
+++ lib/hooks/useSongTracks.tsx
@@ -1,4 +1,3 @@
-import { levenshteinEditDistance } from 'levenshtein-edit-distance'
 import { useQuery } from 'react-query'
 
 import { searchTracks } from '../spotify'
@@ -12,24 +11,9 @@ export type SpotifyTrack = SpotifyApi.TrackObjectFull & {
 }
 
 export const useSongTracks = (token: ServiceJwt, song: Song, limit: number, isStrictMode: boolean) => {
-  const fetcher = async () => {
-    const query = [`track:${song.title}`]
-    if (song.creators.artist) {
-      query.push(`artist:${song.creators.artist}`)
-    }
+  const { data, ...queryResult } = useQuery(['spotify', token, song.id], () => fetcher(token, song))
 
-    const response = await searchTracks(token, query.join(' '), 50)
-    return response.tracks.items
-  }
-
-  const { data, ...queryResult } = useQuery(['spotify', token, song.id, isStrictMode], fetcher)
-  const tracks: SpotifyTrack[] =
-    data?.map((track) => ({
-      ...track,
-      distance: calculateEditDistance(song, track),
-      likelihood: 0,
-    })) ?? []
-
+  const tracks = data ?? []
   const maxDistance = Math.max(...tracks.map((track) => track.distance))
   for (const track of tracks) {
     track.likelihood = Math.round((1 - track.distance / maxDistance) * 100)
@@ -51,9 +35,29 @@ export const useSongTracks = (token: ServiceJwt, song: Song, limit: number, isSt
   }
 }
 
-const calculateEditDistance = (song: Song, track: SpotifyApi.TrackObjectFull): number => {
+const fetcher = async (token: ServiceJwt, song: Song): Promise<SpotifyTrack[]> => {
+  const query = [`track:${song.title}`]
+  if (song.creators.artist) {
+    query.push(`artist:${song.creators.artist}`)
+  }
+
+  const response = await searchTracks(token, query.join(' '), 50)
+  const items = response.tracks.items
+  const promises = items.map((track) => intoSpotifyTrack(song, track))
+  return Promise.all(promises)
+}
+
+const intoSpotifyTrack = async (song: Song, track: SpotifyApi.TrackObjectFull): Promise<SpotifyTrack> => ({
+  ...track,
+  distance: await calculateEditDistance(song, track),
+  likelihood: 0,
+})
+
+const calculateEditDistance = async (song: Song, track: SpotifyApi.TrackObjectFull): Promise<number> => {
+  const { levenshteinEditDistance } = await import('levenshtein-edit-distance')
+
   const distance = levenshteinEditDistance(song.title, track.name)
-  if (!song.creators.artist) {
+  if (!song.creators.artist || track.artists.length === 0) {
     return distance
   }
 
